b3fbcaa9e70265531c1c9faa41916401b53fce03
Shahriar Ahmed <shahriar7ahmed@gmail.com>
2022-11-09 15:52:23 +0600

    Review Added

---
+++ src/pages/Services/AddReview.js
@@ -0,0 +1,84 @@
+import React, { useContext } from "react";
+import { useLoaderData } from "react-router-dom";
+import { toast } from "react-toastify";
+import { AuthContext } from "../../contexts/AuthProvider";
+
+const AddReview = () => {
+  const { _id, title } = useLoaderData();
+  const { user } = useContext(AuthContext);
+
+  const handelReviewAdded = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const userName = user?.displayName;
+    const email = user?.email;
+    const userImg = user?.photoURL;
+    const message = form.message.value;
+
+    const review = {
+      review: _id,
+      reviewName: title,
+      userName,
+      email,
+      message,
+      userImg,
+    };
+
+    fetch("http://localhost:5000/reviews", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(review),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.acknowledged) {
+          toast.success("Review Added Successfully");
+          form.reset();
+        }
+      })
+      .catch((er) => console.error(er));
+  };
+
+  return (
+    <div className="m-10">
+      <form onSubmit={handelReviewAdded}>
+        <h2 className="text-4xl my-6">You are about to review: {title}</h2>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          <input
+            name="name"
+            type="text"
+            placeholder="Name"
+            defaultValue={user?.displayName}
+            className="input input-ghost w-full  input-bordered"
+            readOnly
+          />
+          <input
+            name="email"
+            type="text"
+            placeholder="Your email"
+            defaultValue={user?.email}
+            className="input input-ghost w-full  input-bordered"
+            readOnly
+          />
+        </div>
+        <textarea
+          name="message"
+          className="textarea textarea-bordered h-24 my-4 w-full"
+          placeholder="Your Message"
+          required
+        ></textarea>
+
+        <input
+          className="btn btn-primary btn-outline hover:rounded-full"
+          type="submit"
+          value="Place The Review"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default AddReview;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,7 @@ import Blog from "../pages/blog/Blog";
 import Home from "../pages/Home/Home";
 import Login from "../pages/login-register/Login";
 import Register from "../pages/login-register/Register";
+import AddReview from "../pages/Services/AddReview";
 import Service from "../pages/Services/Service";
 import Services from "../pages/Services/Services";
 import About from "../pages/shared/About";
@@ -30,8 +31,14 @@ const router = createBrowserRouter([
         loader: () => fetch("http://localhost:5000/packages"),
       },
       {
-        path: "service/:id",
+        path: "/service/:id",
         element: <Service />,
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/packages/${params.id}`),
+      },
+      {
+        path: "/give-review/:id",
+        element: <AddReview />,
         loader: ({ params }) =>
           fetch(`http://localhost:5000/packages/${params.id}`),
       },
